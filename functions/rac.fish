6684ad29ff417aa536dbb69bc865a3bbde0d2fd2
Magnus Åsrud <magnusasrud92@gmail.com>
2025-02-07 10:26:58 +0100

    Back to using overmind for Rails

--- functions/rac.fish
+++ functions/rac.fish
@@ -2,5 +2,6 @@ function rac --description="Start the rails console"
   set_color red
   echo "Starting rails console"
   set_color normal
-  bin/docker console
+  bin/rails c
+  #bin/docker console
 end
--- functions/ras.fish
+++ functions/ras.fish
@@ -2,7 +2,8 @@ function ras --description="Start the rails server"
   set_color red
   echo "Starting rails server"
   set_color normal
-  # set -lx OVERMIND_SKIP_ENV true
-  # overmind start -f Procfile.dev
-  bin/docker start
+  
+  set -lx OVERMIND_SKIP_ENV true 
+  overmind start -f Procfile.dev
+  #bin/docker start
 end
