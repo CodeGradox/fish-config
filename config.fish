32b6ba03a607e1e86e78968803b01fc32b984639
Magnus Åsrud <magnusasrud92@gmail.com>
2025-10-17 13:26:18 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -16,6 +16,16 @@ set -gx N_PREFIX "$HOME/n"
 # this variable.
 set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@1.1)"
 
+set -gx HEROKU_ORGANIZATION "laft"
+
+# Fixes the `fork` safety issue in Objective-C applications.
+# https://github.com/rails/rails/issues/38560
+set -gx OBJC_DISABLE_INITIALIZE_FORK_SAFETY "YES"
+
+# Tells overmind to skip loading variables from the .env file.
+# I don't remember why I need this.
+set -gx OVERMIND_SKIP_ENV true
+
 # Load aliases.
 source $HOME/.config/fish/aliases.fish
 
@@ -36,3 +46,11 @@ end
 
 # Disables the default fish greeting.
 set fish_greeting
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+# source ~/.orbstack/shell/init2.fish 2>/dev/null || :
